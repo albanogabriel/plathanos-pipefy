98a801ca73674117e01d7fe1178c1d77eafbbe7a
albanogabriel <albanogabriel33@gmail.com>
2025-05-13 15:09:56 -0300

    :wrench: chore: commit themeProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme'
 import client from '@/src/config/apollo'
 import { ApolloProvider } from '@apollo/client'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -21,13 +18,13 @@ export default function RootLayout() {
 
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      {/* <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}> */}
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+      {/* </ThemeProvider> */}
     </ApolloProvider>
   )
 }
